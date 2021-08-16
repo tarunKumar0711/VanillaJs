4e5a0cdf7a123a3d2b46a25140fffb96a0c8b07e
Tarun Kumar <68737903+tathaastu-tarun@users.noreply.github.com>
2021-08-16 23:12:04 +0530

    js

--- Day-02/script.js
+++ Day-02/script.js
@@ -1,10 +1,22 @@
 const secHand = document.querySelector('.sec');
+const minHand = document.querySelector('.min');
+const hourHand = document.querySelector('.hour');
 function setDate()
 {
     const date = new Date();
     const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+
     const secondRotate = ((seconds/60)*360);
+    const minuteDegree = ((minutes/60)*360);
+    const hourDegree = ((hours/12)*360);
 
     secHand.style.transform =`rotate(${secondRotate}deg)`;
+    minHand.style.transform =`rotate(${minuteDegree}deg)`;
+    hourHand.style.transform =`rotate(${hourDegree}deg)`;
+
+    
 }
 setInterval(setDate,1000);
